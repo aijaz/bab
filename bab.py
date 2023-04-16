78736dd6a36494f134bfcda6333c5bedd168cb0f
Aijaz Ansari <aijaz@users.noreply.github.com>
2023-04-16 09:41:51 -0600

    Added app.run

--- bab.py
+++ bab.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World Three!</p>"
 
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
